f597d2ceef60ae6294b69ecb78abecf8fb70b2c7
Shane Nelson <stgngts@gmail.com>
2015-04-29 03:11:17 -0400

    Split ProcessSpy out to its own file. Plug a memory leak in InjectJoin.

--- ElDorado.cs
+++ ElDorado.cs
@@ -5,73 +5,19 @@ using System.IO;
 using System.Linq;
 using System.Management;
 using System.Net;
-using System.Runtime.InteropServices;
 using System.Text;
 
 namespace CoolRanch
 {
-    class Kernel32
-    {
-        [DllImport("kernel32.dll")]
-        public static extern int OpenProcess(int dwDesiredAccess, bool bInheritHandle, uint dwProcessId);
-
-        [DllImport("kernel32.dll")]
-        public static extern bool CloseHandle(int hObject);
-
-        [DllImport("kernel32.dll")]
-        public static extern bool ReadProcessMemory(int hProcess, int lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
-
-        [DllImport("kernel32.dll")]
-        public static extern bool WriteProcessMemory(int hProcess, int lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesWritten);
-
-        [DllImport("kernel32.dll")]
-        public static extern int VirtualAllocEx(int hProcess, int lpAddress, int dwSize, int flAllocationType, int flProtect);
-    }
-
-    class ProcessSpy
-    {
-        public uint ProcessId;
-        readonly int _handle;
-
-        public ProcessSpy(uint processId)
-        {
-            this.ProcessId = processId;
-            _handle = Kernel32.OpenProcess(0x38, false, processId);
-        }
-
-        ~ProcessSpy()
-        {
-            Kernel32.CloseHandle(_handle);
-        }
-
-        public byte[] Read(int address, int size)
-        {
-            var buf = new byte[size];
-            int bytesread = 0;
-            Kernel32.ReadProcessMemory(_handle, address, buf, size, ref bytesread);
-            return buf.Take(bytesread).ToArray();
-        }
-
-        public int Write(int address, byte[] data)
-        {
-            int byteswritten = 0;
-            Kernel32.WriteProcessMemory(_handle, address, data, data.Length, ref byteswritten);
-            return byteswritten;
-        }
-
-        public int Alloc(int size)
-        {
-            return Kernel32.VirtualAllocEx(_handle, 0, size, 0x3000, 0x40);
-        }
-    }
-
     public class ElDorado
     {
-        public bool IsRunning = false;
+        public bool IsRunning;
         ProcessSpy _gameProcess;
 
         public event EventHandler ProcessLaunched;
         public event EventHandler ProcessClosed;
+
+        private int _syslinkDataBuffer;
 
         void SpyOnGameProcess(uint pid)
         {
@@ -172,16 +118,17 @@ namespace CoolRanch
             writer.Seek(178, SeekOrigin.Current);
             writer.Write(peer.Address.GetAddressBytes().Reverse().ToArray());
             writer.Write((short)peer.Port);
-            writer.BaseStream.Flush();
+            writer.Flush();
 
-            var address = _gameProcess.Alloc(374);
-            _gameProcess.Write(address, syslinkData);
+            if (_syslinkDataBuffer == 0)
+                _syslinkDataBuffer = _gameProcess.Alloc(374);
+            _gameProcess.Write(_syslinkDataBuffer, syslinkData);
 
             var syslinkLenPtr = new byte[8];
             writer = new BinaryWriter(new MemoryStream(syslinkLenPtr));
             writer.Write(1);
-            writer.Write(address);
-            writer.BaseStream.Flush();
+            writer.Write(_syslinkDataBuffer);
+            writer.Flush();
 
             _gameProcess.Write(0x228e6d8, syslinkLenPtr);
 
@@ -194,7 +141,7 @@ namespace CoolRanch
             writer.Seek(16, SeekOrigin.Current);
             writer.Write(rawXnAddr);
             writer.Write(1);
-            writer.BaseStream.Flush();
+            writer.Flush();
 
             _gameProcess.Write(0x2240b98, joinData);
         }
---
+++ ProcessSpy.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace CoolRanch
+{
+    class Kernel32
+    {
+        [DllImport("kernel32.dll")]
+        public static extern int OpenProcess(int dwDesiredAccess, bool bInheritHandle, uint dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool CloseHandle(int hObject);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(int hProcess, int lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool WriteProcessMemory(int hProcess, int lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll")]
+        public static extern int VirtualAllocEx(int hProcess, int lpAddress, int dwSize, int flAllocationType, int flProtect);
+    }
+
+    class ProcessSpy
+    {
+        public uint ProcessId;
+        readonly int _handle;
+
+        public ProcessSpy(uint processId)
+        {
+            this.ProcessId = processId;
+            _handle = Kernel32.OpenProcess(0x38, false, processId);
+        }
+
+        ~ProcessSpy()
+        {
+            Kernel32.CloseHandle(_handle);
+        }
+
+        public byte[] Read(int address, int size)
+        {
+            var buf = new byte[size];
+            var bytesRead = 0;
+            Kernel32.ReadProcessMemory(_handle, address, buf, size, ref bytesRead);
+            return buf.Take(bytesRead).ToArray();
+        }
+
+        public int Write(int address, byte[] data)
+        {
+            var bytesWritten = 0;
+            Kernel32.WriteProcessMemory(_handle, address, data, data.Length, ref bytesWritten);
+            return bytesWritten;
+        }
+
+        public int Alloc(int size)
+        {
+            return Kernel32.VirtualAllocEx(_handle, 0, size, 0x3000, 0x40);
+        }
+    }
+}
